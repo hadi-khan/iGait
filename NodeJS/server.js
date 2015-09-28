34822e8eade09a21ef09b281b5e161f40f52839b
Ross Walker <ross.walker@mavs.uta.edu>
2015-09-27 22:40:32 -0500

    Added skeleton code for API routes.

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -2,12 +2,43 @@
 
 var
   express = require('express'),
-  app = express();
+  app = express(),
+  bodyParser = require('body-parser');
 
-app.get('/', function(req, res){
-  res.send('hello world');
+
+var port = process.env.PORT || 80;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// BASIC ROUTES---------------------------
+
+// Route for every connection.
+app.use(function(req, res, next){
+  console.log('someone is using the api!');
+  next();
 });
 
-app.listen(80, function(){
-  console.log('Listening for connections on port 80');
+app.get('/', function(req, res){
+  res.send('iGait API server!');
+});
+
+// AUTHENTICATION ROUTES------------------
+app.use('/authentication', function(req, res){
+  res.send('Here is your token');
+});
+
+// API ROUTES-----------------------------
+var routes = express.Router();
+
+// Token verification before allowing use of API resources.
+routes.use(function(req, res, next){
+  console.log('This token looks ok, let that user through.');
+  next();
+});
+
+app.use('/api', routes);
+
+// SERVER START---------------------------
+app.listen(port, function(){
+  console.log(`Listening for connections on port ${port}`);
 });
