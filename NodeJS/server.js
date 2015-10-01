7ddf225405182f18fa449d705b75df376a546403
kevko1991 <kevko1991@yahoo.com>
2015-10-01 07:18:54 -0500

    Added doctor schema and have line to create a doctor

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,7 +3,10 @@
 var
   express = require('express'),
   app = express(),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  ObjectId = mongoose.Schema.Types.ObjectId;
 
 
 var port = process.env.PORT || 80;
@@ -36,6 +39,57 @@ routes.use(function(req, res, next){
 });
 
 app.use('/api', routes);
+
+
+//connecting to mongodb with mongoose
+mongoose.connect('mongodb://localhost:27017/mongotest');
+mongoose.connection.on('open', function() {
+console.log('Mongoose connected.');
+});
+
+
+//defining the doctors schema
+var doctorsSchema = new Schema({
+	date_created: { type: Date, default: Date.now },
+	email: { type: String, required: true, unique: true},
+	password: { type: String, required: true},
+	name: {
+	  first: { type: String, required: true, trim: true},
+	  last: { type: String, required: true, trim: true}
+	},
+	contact: { 
+		mobilenumber: { type: Number },
+		officenumber: { type: Number },
+		officeaddress: { type: String }
+	},
+	patients: [{ type : ObjectId, ref: 'Patients' }],
+	accessToken: { type: String } // Used for Remember Me
+});
+
+var DoctorsModel = mongoose.model('Doctors', doctorsSchema);
+exports.Doctors = mongoose.model('Doctors', doctorsSchema)
+
+//creating a new doctor
+var newDoctor = new DoctorsModel({
+	email: 'kevin.kocian@mavs.uta.edu',
+	password: 'password',
+	name: {
+	  first: 'kevin',
+	  last: 'kocian'
+	},
+	contact: { 
+		phonenumber: 2546449059,
+		officenumber: 2546449999,
+		officeaddress: '1100 Greek Row Dr'
+	},
+	patients: []
+});
+
+//save new doctor into the database
+//newDoctor.save();
+
+;
+
 
 // SERVER START---------------------------
 app.listen(port, function(){
