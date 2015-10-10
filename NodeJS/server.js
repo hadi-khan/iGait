36f656458eff5e07eab8b079da93c1af5e3ad0c8
Ross Walker <ross.walker@mavs.uta.edu>
2015-10-10 10:48:09 -0500

    Seperated the api to seperate modules.  Each layer can now be run as its own seperate application once more code is added to the module.

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -4,12 +4,9 @@ var
   express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  ObjectId = mongoose.Schema.Types.ObjectId;
+  routeAPI = require('./routes/api.js');
 
-
-var port = process.env.PORT || 80;
+app.set('port', process.env.PORT || 80);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -26,72 +23,9 @@ app.get('/', function(req, res){
 });
 
 // API ROUTES-----------------------------
-var routes = express.Router();
-
-routes.post('/authentication', function(req, res){
-  res.send('Here is your token');
-});
-
-// Token verification before allowing use of API resources.
-routes.use(function(req, res, next){
-  console.log('This token looks ok, let that user through.');
-  next();
-});
-
-app.use('/api', routes);
-
-
-//connecting to mongodb with mongoose
-mongoose.connect('mongodb://localhost:27017/mongotest');
-mongoose.connection.on('open', function() {
-console.log('Mongoose connected.');
-});
-
-
-//defining the doctors schema
-var doctorsSchema = new Schema({
-	date_created: { type: Date, default: Date.now },
-	email: { type: String, required: true, unique: true},
-	password: { type: String, required: true},
-	name: {
-	  first: { type: String, required: true, trim: true},
-	  last: { type: String, required: true, trim: true}
-	},
-	contact: { 
-		mobilenumber: { type: Number },
-		officenumber: { type: Number },
-		officeaddress: { type: String }
-	},
-	patients: [{ type : ObjectId, ref: 'Patients' }],
-	accessToken: { type: String } // Used for Remember Me
-});
-
-var DoctorsModel = mongoose.model('Doctors', doctorsSchema);
-exports.Doctors = mongoose.model('Doctors', doctorsSchema)
-
-//creating a new doctor
-var newDoctor = new DoctorsModel({
-	email: 'kevin.kocian@mavs.uta.edu',
-	password: 'password',
-	name: {
-	  first: 'kevin',
-	  last: 'kocian'
-	},
-	contact: { 
-		phonenumber: 2546449059,
-		officenumber: 2546449999,
-		officeaddress: '1100 Greek Row Dr'
-	},
-	patients: []
-});
-
-//save new doctor into the database
-//newDoctor.save();
-
-;
-
+app.use(routeAPI);
 
 // SERVER START---------------------------
-app.listen(port, function(){
-  console.log(`Listening for connections on port ${port}`);
+app.listen(app.get('port'), function(){
+  console.log(`Listening for connections on port ${app.get('port')}`);
 });
