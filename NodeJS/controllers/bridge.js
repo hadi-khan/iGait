b31db96fc68148874af436e6ee8c0ee842adfda2
kevko1991 <kevko1991@yahoo.com>
2015-11-25 12:01:23 -0600

    fix remove patient, remove doctor. added update health, remove health, gethealthByObjectID,  and removed getdoctorbyemail

--- NodeJS/controllers/bridge.js
+++ NodeJS/controllers/bridge.js
@@ -8,7 +8,6 @@ var mongoose = require('mongoose'),
     Models = require('../models'),
     Schema = mongoose.Schema,
     ObjectId = mongoose.Schema.Types.ObjectId;
-//command = require('command.js');
 
 function dbMgrBridge(whichdb) {
 // Implementation reference:
@@ -19,19 +18,16 @@ function dbMgrBridge(whichdb) {
 
 dbMgrBridge.prototype = {
     //Private Method:
-    _IAmAPrivateMethod: function(){
-        return true;
-    },
     _SetImplementation: function(implementor) {
         this._impl = null;
         if(implementor) this._impl = implementor;
     },
-    // EstablishImplementor - function that creates            |||||factory method
+    // EstablishImplementor - function that creates
     // the Concrete Implementor and binds it to Abstraction.
     // This is the very method to place your
     // browser/feature/object detection code.
     _EstablishImplementor: function(whichdb) {
-        if(whichdb === 1) {//change later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if(whichdb === 1) {
             return new ImplementationMongoose();//mongoose / mongodb
         }
         else if(false) {
@@ -43,11 +39,6 @@ dbMgrBridge.prototype = {
     // Function "exported" by the Abstraction:
     // for each one of these, add to implmentation 1 and implementation 2
     // Public Methods
-    getDoctorByEmail: function(reqEmail, callback) {
-        // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.getDoctorByEmail)
-            this._impl.getDoctorByEmail(reqEmail, callback);     // Forward request to implementor
-    },
     getDoctorByObjectID: function(reqObjectID, callback){
         // Check if any implementor is bound and has the required method:
         if(this._impl && this._impl.getDoctorByObjectID) {
@@ -56,8 +47,15 @@ dbMgrBridge.prototype = {
     },
     getPatientByObjectID: function(reqObjectID, callback){
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.getPatientByObjectID)
+        if(this._impl && this._impl.getPatientByObjectID){
             this._impl.getPatientByObjectID(reqObjectID, callback);     // Forward request to implementor
+        }
+    },
+    getHealthByObjectID: function(reqObjectID, callback){
+        // Check if any implementor is bound and has the required method:
+        if(this._impl && this._impl.getHealthByObjectID){
+            this._impl.getHealthByObjectID(reqObjectID, callback);     // Forward request to implementor
+        }
     },
     getDoctorPatients: function(reqObjectID, callback){
         // Check if any implementor is bound and has the required method:
@@ -73,66 +71,83 @@ dbMgrBridge.prototype = {
     },
     createDoctor: function(reqDocObj, callback) {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.createDoctor)
+        if(this._impl && this._impl.createDoctor){
             this._impl.createDoctor(reqDocObj, callback);     // Forward request to implementor
+        }
     },
     createPatient: function(reqPatObj, callback) {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.createPatient)
+        if(this._impl && this._impl.createPatient){
             this._impl.createPatient(reqPatObj, callback);     // Forward request to implementor
+        }
     },
     createHealth: function(reqHeaObj, callback) {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.createHealth)
+        if(this._impl && this._impl.createHealth){
             this._impl.createHealth(reqHeaObj, callback);     // Forward request to implementor
+        }
     },
     updateDoctor: function(reqObjectID, reqUpdate, callback){
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.updateDoctor)
+        if(this._impl && this._impl.updateDoctor){
             this._impl.updateDoctor(reqObjectID, reqUpdate, callback);     // Forward request to implementor
+        }
     },
     updatePatient: function(reqObjectID, reqUpdate, callback){
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.updatePatient)
+        if(this._impl && this._impl.updatePatient){
             this._impl.updatePatient(reqObjectID, reqUpdate, callback);     // Forward request to implementor
+        }
     },
-    removeDoctor: function(reqEmail, callback) {
+    updateHealth: function(reqObjectID, reqUpdate, callback){
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.removeDoctor)
-            this._impl.removeDoctor(reqEmail, callback);     // Forward request to implementor
+        if(this._impl && this._impl.updateHealth){
+            this._impl.updateHealth(reqObjectID, reqUpdate, callback);     // Forward request to implementor
+        }
     },
-    removePatient: function(reqEmail, callback) {
+    removeDoctor: function(reqObjectID, callback) {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.removePatient)
-            this._impl.removePatient(reqEmail, callback);     // Forward request to implementor
+        if(this._impl && this._impl.removeDoctor){
+            this._impl.removeDoctor(reqObjectID, callback);     // Forward request to implementor
+        }
+    },
+    removePatient: function(reqObjectID, callback) {
+        // Check if any implementor is bound and has the required method:
+        if(this._impl && this._impl.removePatient){
+            this._impl.removePatient(reqObjectID, callback);     // Forward request to implementor
+        }
+    },
+    removeHealth: function(reqObjectID, callback) {
+        // Check if any implementor is bound and has the required method:
+        if(this._impl && this._impl.removeHealth){
+            this._impl.removeHealth(reqObjectID, callback);     // Forward request to implementor
+        }
     },
     connectDB: function() {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.connectDB)
+        if(this._impl && this._impl.connectDB){
             this._impl.connectDB();     // Forward request to implementor
-
+        }
     },
     disconnectDB: function() {
         // Check if any implementor is bound and has the required method:
-        if(this._impl && this._impl.disconnectDB)
+        if(this._impl && this._impl.disconnectDB){
             this._impl.disconnectDB();     // Forward request to implementor
+        }
     }
 };
 
 //Concrete Implementation
 // This is the first in the set of concrete implementors:
-function ImplementationMongoose()
-{
+function ImplementationMongoose() {
     //implement private methods
     //need to implement command pattern to this
     this._dbAddress = 'mongodb://localhost:27017/mongotest';
     this._dbConnectedMessage = "Mongoose connected on localhost:27017";
     return this;
 }
-
 ImplementationMongoose.prototype = {
-    connectDB: function()
-    {
+    connectDB: function() {
         //connecting to mongodb with mongoose
         mongoose.connect(this._dbAddress);
         mongoose.connection.on('open', function() {
@@ -151,8 +166,7 @@ ImplementationMongoose.prototype = {
             });
         });
     },
-    disconnectDB: function()
-    {
+    disconnectDB: function() {
         process.on('SIGINT', function() {
             mongoose.connection.close(function () {
                 console.log('moongose default connection disconnected through app termination');
@@ -161,11 +175,6 @@ ImplementationMongoose.prototype = {
         });
         mongoose.connection.close();
     },
-    getDoctorByEmail: function(reqEmail, callback){
-        Models.Doctors.findOne({email:reqEmail}, function(err,doc){
-            callback(err,doc);
-        });
-    },
     getDoctorByObjectID: function(reqObjectID, callback){
         reqObjectID = ObjectId(reqObjectID);
         Models.Doctors.findOne({_id:reqObjectID}, function(err,doc){
@@ -173,10 +182,15 @@ ImplementationMongoose.prototype = {
         });
     },
     getPatientByObjectID: function(reqObjectID, callback){
-        //TODO Had to edit this mehtod as well.
         reqObjectID = ObjectId(reqObjectID);
         Models.Patients.findOne({_id:reqObjectID}, function(err,pat){
             callback(err,pat);
+        });
+    },
+    getHealthByObjectID: function(reqObjectID, callback){
+        reqObjectID = ObjectId(reqObjectID);
+        Models.Health.findOne({_id:reqObjectID}, function(err,hea){
+            callback(err,hea);
         });
     },
     getDoctorPatients: function(reqObjectID, callback){
@@ -190,8 +204,8 @@ ImplementationMongoose.prototype = {
     getPatientsHealth: function(reqObjectID, callback){
         //may need to use next line
         //reqObjectID = ObjectId(reqObjectID);
-        Models.Health.find({patient:reqObjectID}, function(err,pat){
-            callback(err,pat);
+        Models.Health.find({patient:reqObjectID}, function(err,hea){
+            callback(err,hea);
         });
     },
     createPatient: function(reqPatObj, callback){
@@ -221,32 +235,43 @@ ImplementationMongoose.prototype = {
             callback(err,pat);
         });
     },
-    removeDoctor: function(reqEmail, callback){
-        Models.Doctors.findOneAndRemove({email: reqEmail}, function(err, doc){
+    updateHealth: function(reqObjectID, reqUpdate, callback){
+        //reqObjectID = ObjectId(reqObjectID)
+        Models.Health.findOneAndUpdate({_id: reqObjectID}, reqUpdate, function(err, hea) {
+            callback(err,hea);
+        });
+    },
+    removeDoctor: function(reqObjectID, callback){
+        //reqObjectID = ObjectId(reqObjectID)
+        Models.Doctors.findOneAndRemove({_id: reqObjectID}, function(err, doc){
             callback(err,doc);
         });
     },
-    removePatient: function(reqEmail, callback){
-        Models.Patients.findOneAndRemove({email: reqEmail}, function(err, pat){
+    removePatient: function(reqObjectID, callback){
+        //reqObjectID = ObjectId(reqObjectID)
+        Models.Patients.findOneAndRemove({_id: reqObjectID}, function(err, pat){
             callback(err, pat);
+        });
+    },
+    removeHealth: function(reqObjectID, callback){
+        //reqObjectID = ObjectId(reqObjectID)
+        Models.Health.findOneAndRemove({_id: reqObjectID}, function(err, hea){
+            callback(err, hea);
         });
     }
 };
 
 //Concrete Implementation 2//
 // This is the second implementor:
-function ImplementationTwo()
-{
+function ImplementationTwo() {
     this._dbAddress = '';
     this._dbConnectedMessage = "";
     return this;
 }
 ImplementationTwo.prototype = {
-    connectDB: function()
-    {
+    connectDB: function() {
     },
-    disconnectDB: function()
-    {
+    disconnectDB: function() {
     }
 };
 module.exports = {
